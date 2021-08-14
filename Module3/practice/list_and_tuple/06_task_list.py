4d1d334b2949fa659054d4212a35ffbbcd24ad20
Humorles12 <88583646+Humorles12@users.noreply.github.com>
2021-08-14 16:08:38 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -1,7 +1,6 @@
-# Даны два целых числа.
-# Вывести список всех чисел кратных трем в диапазоне между заданными числами.
+first_number = int(input("Введите 1 число: "))
+second_number = int(input("Введите 2 число: "))
 
-first_number = int(input())     # Первое число
-second_number = int(input())    # Второе число
-
-# TODO: your code here
+for i in range(first_number, second_number + 1):
+    if i % 3 == 0:
+        print(i)
