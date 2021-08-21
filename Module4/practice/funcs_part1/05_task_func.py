d9d37b5b7924de3215f15699b542ea6c4f53bb24
Humorles12 <88583646+Humorles12@users.noreply.github.com>
2021-08-21 15:25:21 +0300

    Update 05_task_func.py

--- Module4/practice/funcs_part1/05_task_func.py
+++ Module4/practice/funcs_part1/05_task_func.py
@@ -8,7 +8,7 @@ def can_triangle(p1, p2, p3):
     c = distance(*p2, *p3)
     if a + b > c and a + c > b and b + c > a:
         p = a + b + c
-        s = (p * (p - a) * (p - b) * (p - c)) ** 0, 5
+        s = (p * (p - a) * (p - b) * (p - c)) ** 0.5
         return s
     else:
         return "Такого треугольника не существует."
